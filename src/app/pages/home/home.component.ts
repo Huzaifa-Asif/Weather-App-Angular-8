43b8232b062c1c4bb9ed5db43719f8dacd6e97f1
Huzaifa Asif <huzaifa@theaweb.net>
2021-02-04 12:27:46 +0500

    passing data to component and displaying weather

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -22,6 +22,8 @@ export class HomeComponent implements OnInit {
   helsinkiData;
   dublinData;
   vancouverData;
+  weatherData = [];
+  locationArray = [2344116, 638242, 44418, 565346, 560743, 9807];
   constructor(
     private route: Router,
     private toastrService: ToastrService,
@@ -30,49 +32,64 @@ export class HomeComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.getWeatherData();
+    // this.getWeatherData();
+    this.getWeather();
+  }
+
+  getWeather(){
+    let i=0;
+    for(i; i<this.locationArray.length; i++){
+      this.api.get('location/'+this.locationArray[i]).then((response: any) => {
+        this.weatherData.push(response);
+        console.log(response);
+      });
+    }
+
+    console.log("weather Data")
+    console.log(this.weatherData)
+
   }
 
   getWeatherData() {
     // Get Istanbul Weather
     this.api.get('location/2344116/').then((response: any) => {
       this.istanbulData = response;
+      this.weatherData.push(response);
       console.log(this.istanbulData);
     });
 
     // Get Berlin Weather
     this.api.get('location/638242/').then((response: any) => {
       this.berlinData = response;
+      this.weatherData.push(response);
       console.log(this.berlinData);
     });
 
     // Get London Weather
     this.api.get('location/44418/').then((response: any) => {
       this.londonData = response;
+      this.weatherData.push(response);
       console.log(this.londonData);
     });
 
     // Get Helsinki Weather
     this.api.get('location/565346/').then((response: any) => {
       this.helsinkiData = response;
-      console.log(this.helsinkiData);
-    });
-
-    // Get Helsinki Weather
-    this.api.get('location/565346/').then((response: any) => {
-      this.helsinkiData = response;
+      this.weatherData.push(response);
       console.log(this.helsinkiData);
     });
 
     // Get Dublin Weather
     this.api.get('location/560743/').then((response: any) => {
       this.dublinData = response;
+      this.weatherData.push(response);
       console.log(this.dublinData);
     });
 
     // Get Vancouver Weather
     this.api.get('location/9807/').then((response: any) => {
       this.vancouverData = response;
+      this.weatherData.push(response);
       console.log(this.vancouverData);
     });
 
--- src/app/pages/weather/weather.component.ts
+++ src/app/pages/weather/weather.component.ts
@@ -1,10 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 declare var $: any;
 import { RestApiService } from '../../services/api/rest-api.service';
-import { Observable } from 'rxjs';
-import Swal from 'sweetalert2';
 import { NgxSpinnerService } from "ngx-spinner";
 @Component({
   selector: 'app-weather',
@@ -17,62 +15,17 @@ export class WeatherComponent implements OnInit {
     title: ''
   };
 
+  @Input() weatherData: [];
+
   constructor(
     private route: Router,
     private toastrService: ToastrService,
-    private api: RestApiService,
-    private spinner: NgxSpinnerService,
   ) { }
 
   ngOnInit() {
-    // this.getWeatherData();
+    console.log("weather Component")
+    console.log(this.weatherData)
   }
-
-  // getWeatherData() {
-  //   // Get Istanbul Weather
-  //   this.api.get('location/2344116/').then((response: any) => {
-  //     this.istanbulData = response;
-  //     console.log(this.istanbulData);
-  //   });
-
-  //   // Get Berlin Weather
-  //   this.api.get('location/638242/').then((response: any) => {
-  //     this.berlinData = response;
-  //     console.log(this.berlinData);
-  //   });
-
-  //   // Get London Weather
-  //   this.api.get('location/44418/').then((response: any) => {
-  //     this.londonData = response;
-  //     console.log(this.londonData);
-  //   });
-
-  //   // Get Helsinki Weather
-  //   this.api.get('location/565346/').then((response: any) => {
-  //     this.helsinkiData = response;
-  //     console.log(this.helsinkiData);
-  //   });
-
-  //   // Get Helsinki Weather
-  //   this.api.get('location/565346/').then((response: any) => {
-  //     this.helsinkiData = response;
-  //     console.log(this.helsinkiData);
-  //   });
-
-  //   // Get Dublin Weather
-  //   this.api.get('location/560743/').then((response: any) => {
-  //     this.dublinData = response;
-  //     console.log(this.dublinData);
-  //   });
-
-  //   // Get Vancouver Weather
-  //   this.api.get('location/9807/').then((response: any) => {
-  //     this.vancouverData = response;
-  //     console.log(this.vancouverData);
-  //   });
-
-
-  // }
 
 
   viewWeatherDetail(woeid) {
